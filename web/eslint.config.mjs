2642b4369230eeaa14a4b3b97aa40c115cc1bd1a
Simon Carlsson <operator_ax.09@icloud.com>
2026-02-20 14:01:49 +0100

    ESLint config

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -3,16 +3,36 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+	...nextVitals,
+	...nextTs,
+	// Override default ignores of eslint-config-next.
+	globalIgnores([
+		// Default ignores of eslint-config-next:
+		".next/**",
+		"out/**",
+		"build/**",
+		"next-env.d.ts",
+	]),
+	{
+		rules: {
+			// Enforce consistent indentation of 2 spaces.
+			indent: ["error", 2],
+      // Disallow var
+      "no-var": "error",
+      // Require the use of === and !== instead of == and !=.
+      "eqeqeq": "error",
+			// Disallow logging of console messages.
+			"no-console": "error",
+			// Enforce the use of `const` for variables that are never reassigned after declaration.
+			"prefer-const": "error",
+			// Disallow the use of `any` type in TypeScript.
+			"@typescript-eslint/no-explicit-any": "error",
+			// Disallow unused variables in TypeScript.
+			"@typescript-eslint/no-unused-vars": "error",
+			// Enforce consistent usage of type imports in TypeScript.
+			"@typescript-eslint/consistent-type-imports": "error",
+		},
+	},
 ]);
 
 export default eslintConfig;
