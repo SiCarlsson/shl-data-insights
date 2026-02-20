e3f837a72d17e1363aa635cfb35a3f18838896b1
Simon Carlsson <operator_ax.09@icloud.com>
2026-02-20 17:24:48 +0100

    Add termsOfService page and privacy page

---
+++ web/app/[locale]/privacy/page.tsx
@@ -0,0 +1,15 @@
+import { getTranslations, setRequestLocale } from "next-intl/server";
+
+export default async function Privacy({ params }: { params: Promise<{ locale: string }> }) {
+  const { locale } = await params;
+  setRequestLocale(locale);
+
+  const t = await getTranslations("PrivacyPage");
+
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+      <p>{t("description")}</p>
+    </div>
+  );
+}
